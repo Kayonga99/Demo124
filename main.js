70412895aa2eb95bf22b569209a237f26ef5b222
Walker4real <frankkafweku84@gmail.com>
2023-02-13 07:22:10 -0800

    add an event listener

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 // const lable = document.querySelectorAll
 const openMenu = document.querySelector(".parent")
-console.log(openMenu);
+// console.log(openMenu);
+const closeMenu = document.querySelector(".close")
+
+openMenu.addEventListener("click")
