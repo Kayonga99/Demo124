1358eb3930053ca8a9b6b25412f9990c1821ed05
Mwambu Mulonda <mwambumulonda@Mwambus-Mac-mini.local>
2023-02-13 18:16:47 +0200

    Update branch.

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
 // const lable = document.querySelectorAll
-const popUpp = document.querySelector(".menu")
-const openMenu = document.querySelector(".parent")
+const popUpp = document.querySelector('.menu');
+const openMenu = document.querySelector('.parent');
 // console.log(openMenu);
-const closeMenu = document.querySelector(".close")
+const closeMenu = document.querySelector('.close');
 
+openMenu.addEventListener('click', () => {
+  popUpp.style.display = 'flex';
+  openMenu.style.display = 'none';
+});
 
-openMenu.addEventListener("click", () => {
-    popUpp.style.display = "flex";
-    openMenu.style.display = "none"
-})
-
-
-
-closeMenu.addEventListener("click", () => {
-    popUpp.style.display = "none"; 
-    openMenu.style.display = "flex"
-
-})
+closeMenu.addEventListener('click', () => {
+  popUpp.style.display = 'none';
+  openMenu.style.display = 'flex';
+});
